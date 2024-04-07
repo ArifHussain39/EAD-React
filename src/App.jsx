5a58a77e77eca7ba48da8c06205f441d492d12af
Arif Hussain <arif3337179@gmail.com>
2024-04-08 00:05:41 +0500

    x

--- src/App.jsx
+++ src/App.jsx
@@ -1,61 +1,18 @@
 import * as React from 'react';
-import Stack from '@mui/material/Stack';
-import CircularProgress from '@mui/material/CircularProgress';
 import './App.css';
 
 export default function CircularDeterminate() {
-  const [progress, setProgress] = React.useState(0);
-
-  React.useEffect(() => {
-    const timer = setInterval(() => {
-      setProgress((prevProgress) => (prevProgress >= 100 ? 0 : prevProgress + 10));
-    }, 800);
-
-    return () => {
-      clearInterval(timer);
-    };
-  }, []);
 
   return (
-    <Stack className='div' spacing={2} direction="row">
-      <CircularProgress variant="determinate" value={progress} />
-    </Stack>
+    <div className="App">
+      <div className="App-header">
+        <h2>My npm package</h2>
+      </div>
+      <div className="App-body">
+        <div className="progress-bar">
+          <div className="progress-bar-fill"></div>
+        </div>
+      </div>
+    </div>
   );
 }
-
-// import { useState } from 'react'
-// import './App.css'
-
-// function App() {
-//   const [count, setCount] = useState(0);
-
-//   let products = [
-//     { id: 1, name: "product 1" },
-//     { id: 2, name: "product 2" },
-//     { id: 3, name: "product 3" },
-//     { id: 4, name: "product 4" },
-//     { id: 5, name: "product 5" },
-//   ];
-  
-
-//   return (
-//     <>
-
-//       <ul>
-//         {
-//          products?.map(product => <li key={product.id} > {product.name} </li>)
-
-//         }
-//       </ul>
-
-//       {
-//         products ? <h1>Products</h1> : <h1>No Products</h1>
-//       }
-
-
-
-//     </>
-//   )
-// }
-
-// export default App
