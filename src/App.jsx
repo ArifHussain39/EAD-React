5efbc14e94b6eaeced95f0547118024dadd6e2ea
Arif Hussain <arif3337179@gmail.com>
2024-04-19 09:33:53 +0500

    .

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,7 @@ export default function CircularDeterminate() {
 
   return (
     <div className="App">
-      <div className="App-header">
-        <h2>My npm package</h2>
-      </div>
-      <div className="App-body">
-        <div className="progress-bar">
-          <div className="progress-bar-fill"></div>
-        </div>
-      </div>
+     
     </div>
   );
 }
