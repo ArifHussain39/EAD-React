69b294c7477d73fd6dae088b2f122e361f719c7d
Arif Hussain <arif3337179@gmail.com>
2024-04-21 18:01:43 +0500

    /

--- src/AOS.jsx
+++ src/AOS.jsx
@@ -5,27 +5,34 @@ import img3 from './assets/images/AOS/download3.jpg';
 import img4 from './assets/images/AOS/download4.jpg';
 import img5 from './assets/images/AOS/download5.jpg';
 import AOs from 'aos';
-import 'aos/dist/aos.css'; 
+import 'aos/dist/aos.css';
 
 
 function AOS() {
 
-    AOs.init({duration: 2000});
+    AOs.init();
 
     return (
         <>
 
             <h1>AOS Animations</h1>
             <div >
-                <img data-aos="fade-right" src={img1} alt="AOS" width={'600px'}/>
+                <img data-aos="fade-right" src={img1} alt="AOS" width={'600px'} />
                 <br />
-                <img data-aos="fade-left" src={img2} alt="AOS" width={'600px'}/>
-                <br /> 
-                <img data-aos="fade-up" src={img3} alt="AOS" width={'600px'}/>
+                <img data-aos="fade-left" src={img2} alt="AOS" width={'600px'} />
                 <br />
-                <img data-aos="fade-in" src={img4} alt="AOS" width={'600px'}/>
+                <img data-aos="fade-up" src={img3} alt="AOS" width={'600px'} />
                 <br />
-                <img data-aos="fade-right" src={img5} alt="AOS" width={'600px'}/>
+                <img data-aos="fade-in" src={img4} alt="AOS" width={'600px'} />
+                <br />
+                <img data-aos="fade-up"
+                    data-aos-offset="200"
+                    data-aos-delay="50"
+                    data-aos-duration="1000"
+                    data-aos-easing="ease-in-out"
+                    data-aos-mirror="true"
+                    data-aos-once="false" src={img5} alt="AOS" width={'600px'} 
+                />
 
             </div>
 
