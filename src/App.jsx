402285e2065c09de8a5b905b67eb467603afd090
Arif Hussain <arif3337179@gmail.com>
2024-05-12 22:07:44 +0500

    react redux

--- src/App.jsx
+++ src/App.jsx
@@ -2,31 +2,10 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  let products = [
-    { id: 1, name: "product 1" },
-    { id: 2, name: "product 2" },
-    { id: 3, name: "product 3" },
-    { id: 4, name: "product 4" },
-    { id: 5, name: "product 5" },
-  ];
-  
 
   return (
     <>
-
-      <ul>
-        {
-         products?.map(product => <li key={product.id} > {product.name} </li>)
-
-        }
-      </ul>
-
-      {
-        products ? <h1>Products</h1> : <h1>No Products</h1>
-      }
-
+      <h1>React App</h1>
 
     </>
   )
