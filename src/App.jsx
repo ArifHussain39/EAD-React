586885220b904f118a5122d16fb6ffab44b3215a
Arif Hussain <arif3337179@gmail.com>
2024-05-21 16:35:09 +0500

    client of CRUD

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
-import { useState } from 'react'
-import './App.css'
+import { useState, useEffect } from "react";
+import "./App.css";
+import axios from "axios";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Users from "./Users";
+import Create from "./Create";
+import Update from "./Update";
+
 
 function App() {
-
   return (
     <>
-      <h1>React App</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Users />} />
+          <Route path="/create" element={<Create />} />
+          <Route path="/update/:id" element={<Update />} />
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
